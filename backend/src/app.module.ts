da6887214e58764d8075c975b0e551602af317c9
karpushh <bnymnkrkbk@gmail.com>
2025-08-01 16:39:27 +0300

    Configured .env for database in backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,25 +5,29 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { User } from './entities/user.entity';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { IdeasModule } from './ideas/ideas.module';
 import { Idea } from './entities/idea.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      database: 'money',
-      username: 'postgres',
-      password: 'karpush987',
-      entities: [User, Idea], //entities you want to register
-      synchronize: true, //dev
-    }),
     ConfigModule.forRoot({
       isGlobal: true, // This makes the ConfigService available everywhere
       envFilePath: '.env', // Specifies the file to load
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule], // Import ConfigModule here as well
+      inject: [ConfigService], // Inject the ConfigService
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [User, Idea],
+        synchronize: true, // Should be false in production
+      }),
     }),
     UsersModule,
     AuthModule,
